7944248bbe7869699721187055943e7ab064ab33
Thomas Fillion <fillion.thomas@gmail.com>
2023-03-10 13:16:54 +0100

    update : use an entropy factor to produce the winner index

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,14 +1,13 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Lottery {
     address public manager;
+    mapping(address => uint) public investments;
     address payable[] public players;
-
 
     event NewPlayer(address player);
     event Winner(address winner, uint amount);
-    uint totalAmount = 0;
+    uint public totalAmount = 0;
 
     constructor() {
         manager = msg.sender;
@@ -18,17 +17,45 @@ contract Lottery {
         require(msg.value > 0.0001 ether);
         totalAmount += msg.value;
         players.push(payable(msg.sender));
+        investments[msg.sender] += msg.value;
         emit NewPlayer(msg.sender);
     }
 
     function pickWinner() public restricted {
         require(players.length > 0);
-        uint index = random() % players.length;
+        uint index = generateRandomIndex();
         address payable winner = players[index];
-        uint amount = address(this).balance;
+        uint amount = totalAmount;
         winner.transfer(amount);
-        players = new address payable[](0);
+        totalAmount = 0;
+        reset();
         emit Winner(winner, amount);
+    }
+
+    function generateRandomIndex() private view returns (uint) {
+        uint totalInvestments = 0;
+        for (uint256 i = 0; i < players.length; i++) {
+            totalInvestments += investments[players[i]];
+        }
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, totalInvestments)));
+        uint256 index = 0;
+        uint256 previousPercentage = 0;
+        for (uint256 i = 0; i < players.length; i++) {
+            uint256 playerPercentage = investments[players[i]] * 100 / totalInvestments;
+            if (randomNumber >= previousPercentage && randomNumber < (previousPercentage + playerPercentage)) {
+                index = i;
+                break;
+            }
+            previousPercentage += playerPercentage;
+        }
+        return index;
+    }
+
+    function reset() private {
+        for (uint256 i = 0; i < players.length; i++) {
+            investments[players[i]] = 0;
+        }
+        players = new address payable[](0);
     }
 
     function getPlayers() public view returns (address payable[] memory) {
@@ -37,10 +64,6 @@ contract Lottery {
 
     function getTotalAmount() public view returns (uint){
         return totalAmount;
-    }
-
-    function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
     }
 
     modifier restricted() {
